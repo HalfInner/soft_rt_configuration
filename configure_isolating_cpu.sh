9ed8e84a1220b84b0cc0ad5e5751c7af0c2754a3
Half's Inner <kajbrz@hotmail.com>
2022-03-27 22:35:09 +0200

    work1

--- configure_isolating_cpu.sh
+++ configure_isolating_cpu.sh
@@ -1,1 +1,30 @@
-cat /boot/cmdline.txt
+#! bin/sh 
+# For more info: https://github.com/raspberrypi/linux/blob/rpi-3.2.27/Documentation/kernel-parameters.txt
+
+function usage() {
+    echo "Usage:"
+    echo "      $0 <test|confiure>"
+}
+
+option=$1
+if [[ "$opition" -eq "test" ]]; then
+    current_config=$(cat /boot/cmdline.txt)
+    echo "Current config: '$current_config'"
+
+    current_isolated_cpu=$(cat /sys/devices/system/cpu/isolated)
+    echo "Current isolated cpu: '$current_isolated_cpu'"
+elif [[ "$opition" -eq "configure" ]] 
+    user_who=$(whoami)
+    if [[ "user_who" -eq "root" ]]; then 
+        arg_isolate_cpu="isolcpus=3"
+        echo "$arg_isolate_cpu" >> /boot/cmdline.txt
+        echo "Added isolated CPU"
+    else
+        echo "Script has to be executed by the root"
+    fi 
+else 
+    usage()
+    exit -1
+fi
+
+
