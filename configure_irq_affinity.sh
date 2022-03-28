8be465d39ea9aad88cc936a3940526fa695fdd17
Half's Inner <kajbrz@hotmail.com>
2022-03-29 00:19:02 +0200

    work1

--- configure_irq_affinity.sh
+++ configure_irq_affinity.sh
@@ -11,7 +11,8 @@ if [[ "$option" == "test" ]]; then
     for irq in /proc/irq/* ; do
         [ -L "${d%/}" ] && continue
         echo "IRQ Affinities:"
-        echo "IRQ${d}: $(cat "$d/smp_affinity_list)"
+        irq_affinity_list=$(cat $d/smp_affinity_list)
+        echo "IRQ${d}: $irq_affinity_list"
     done
 elif [[ "$option" == "configure" ]]; then
     echo "NotSupported"
