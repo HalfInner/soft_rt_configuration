d0c1074173fc1366912a030ab5e3c58252176c93
Half's Inner <kajbrz@hotmail.com>
2022-03-29 00:32:53 +0200

    work1

--- configure_irq_affinity.sh
+++ configure_irq_affinity.sh
@@ -10,7 +10,7 @@ option=$1
 if [[ "$option" == "test" ]]; then
     echo "IRQ Affinities:"
     for irq in "/proc/irq/*" ; do
-        [ ! -f "${irq}/smp_affinity_list" ] && continue
+        #[ -f "${irq}/smp_affinity_list" ] && continue
         irq_affinity_list=$(cat $irq/smp_affinity_list)
         echo "IRQ${irq}: $irq_affinity_list"
     done
