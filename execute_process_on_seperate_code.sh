8215c852cd934be31258dd3ab402bd849cb02fc3
Half's Inner <kajbrz@hotmail.com>
2022-03-30 20:57:17 +0200

    work1

---
+++ execute_process_on_seperate_code.sh
@@ -0,0 +1,24 @@
+#! /bin/sh 
+# For more info: https://github.com/raspberrypi/linux/blob/rpi-3.2.27/Documentation/kernel-parameters.txt
+
+function usage() {
+    echo "Usage:"
+    echo "      $0 <test|configure> <program>"
+}
+
+if [[ "$#" -eq 0 ]]; then
+    echo "Not enough arguments 0"
+    usage
+    exit 1
+fi
+
+option=$1
+program=$2
+defualt_isolated_cpu=3
+if [[ "$option" == "test" ]]; then
+    echo "Not Implemented"
+elif [[ "$option" == "configure" ]]; then
+    taskset -c $defualt_isolated_cpu $option2
+else 
+    usage
+fi
