f67e827e9a7b5b6a7bdb87119dcffb28478657bc
Half's Inner <kajbrz@hotmail.com>
2022-04-26 00:03:34 +0200

    work1

--- tasks/build.sh
+++ tasks/build.sh
@@ -1,15 +1,16 @@
 #! /bin/sh
+
+SCRIPT_PATH=$(dirname "$0")
 FLAGS='-Wall -Werror -Wpedantic -O3'
+mkdir -p out
 printf "Building task_server... "
-g++ --std=c++2a $FLAGS -o task_server.a   task_print_server_high_prio.cc  -lrt
+g++ --std=c++2a $FLAGS -o out/task_server.a  $SCRIPT_PATH/task_print_server_high_prio.cc  -lrt &&\
 printf "Done\n"
 
 printf "Building task_client_a... "
-g++ --std=c++2a $FLAGS -o task_client_a.a   task_print_client_a_lower_prio.cc  -lrt
+g++ --std=c++2a $FLAGS -o out/task_client_a.a $SCRIPT_PATH/task_print_client_a_lower_prio.cc  -lrt &&\
 printf "Done\n"
 
 printf "Building task_client_b... "
-g++ --std=c++2a $FLAGS -o task_client_b.a   task_print_client_b_lower_prio.cc  -lrt
+g++ --std=c++2a $FLAGS -o out/task_client_b.a $SCRIPT_PATH/task_print_client_b_lower_prio.cc  -lrt &&\
 printf "Done\n"
-
-echo $PWD
