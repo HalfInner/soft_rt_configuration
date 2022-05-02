9297d3d221129653c93ad054bff687cd8afe0aa2
Half's Inner <kajbrz@hotmail.com>
2022-05-02 12:32:21 +0200

    Fix chart

--- plot_tracer.py
+++ plot_tracer.py
@@ -4,51 +4,11 @@ import matplotlib.pyplot as plt
 import sys
 
 
-def conv_trace_result_from_file(file: str):
-    timestamp_per_process = {}
-    process_idx = 0
-    timestamp_id = 2
-    event_id = 4
-    min_timestamp = 0xffffffffffffffffffffffffffffffffffff
-    max_timestamp = -1
-    with open(file) as f:
-        first_line = True
-        second_line = False
-        for line in f:
-            if first_line:
-                first_line = False
-                second_line = True
-                continue
-            data = line.split()
-            process = data[process_idx]
-            timestamp = data[timestamp_id]
-            event = event = data[event_id]
-            if second_line:
-                second_line = False
-                event = data[event_id-1]
-            if event != "sched_wakeup:":
-                continue
-
-            curr_timestamps = []
-            if process in timestamp_per_process:
-                curr_timestamps = timestamp_per_process[process]
-            timestamp = float(timestamp.replace(':', ''))
-            curr_timestamps.append(timestamp)
-            min_timestamp = min(timestamp, min_timestamp)
-            max_timestamp = max(timestamp, max_timestamp)
-            timestamp_per_process[process] = curr_timestamps
-
-    return timestamp_per_process, min_timestamp, max_timestamp
-
-
 class ProcessChart:
-    IDX = 0
     def __init__(self, name: str):
-        self.__y_offset = ProcessChart.IDX
-        # ProcessChart.IDX += 1
         self.__process_name = name
         self.__axis_x = [0]
-        self.__axis_y = [self.__y_offset]
+        self.__axis_y = [0]
 
     def turn_on(self, timestamp: float):
         self.__activate(False, timestamp)
@@ -72,63 +32,70 @@ class ProcessChart:
         if self.__process_name == "<idle>-0":
             print(timestamp, self.__axis_x[-1])
 
-        self.__axis_y.append(self.__y_offset + int(not to_zero))
+        self.__axis_y.append(int(not to_zero))
         self.__axis_x.append(timestamp)
         if self.__process_name == "<idle>-0":
             print(timestamp, self.__axis_x[-1])
 
-def conv_trace_from_set_to_data_plot(data):
-    process_charts = []
-    process_indicies = []
-    keys = list(data.keys())
-    for key in keys:
-        process_charts.append(ProcessChart(key))
-        process_indicies.append(0)
 
-    any_process_not_empty = True
-    while (any_process_not_empty):
-        any_process_not_empty = False
-        key_idx = 0
-        min_float = 0xffffffffffffffffffffffff
-        min_key_idx = -1
-        for key, val in data.items():
-            idx = process_indicies[key_idx]
-            chart = process_charts[key_idx]
-            if key != chart.get_name():
-                raise "Key does not match chart"
-            if idx < len(val):
-                any_process_not_empty = True
-                timestamp = val[idx]
-                if timestamp < min_float:
-                    min_float = timestamp
-                    min_key_idx = key_idx
-            key_idx += 1
+def conv_trace_result_from_file(file: str):
+    processes = {}
+    process_idx = 0
+    timestamp_id = 2
+    event_id = 4
+    min_timestamp = 0xffffffffffffffffffffffffffffffffffff
+    max_timestamp = -1
+    with open(file) as f:
+        first_line = True
+        second_line = False
+        for line in f:
+            if first_line:
+                first_line = False
+                second_line = True
+                continue
+            data = line.split()
+            process = data[process_idx].split('-')[0]
+            timestamp = float(data[timestamp_id].replace(':', ''))
+            min_timestamp = min(timestamp, min_timestamp)
+            max_timestamp = max(timestamp, max_timestamp)
+            event = event = data[event_id]
+            if second_line:
+                second_line = False
+                event = data[event_id-1]
 
-        for idx, process in enumerate(process_charts):
-            if idx == min_key_idx:
-                process.turn_on(min_float)
-                process_indicies[min_key_idx] += 1
-            else:
-                process.turn_off(min_float)
-    return process_charts
+            if event == "sched_wakeup:":
+                curr_process = processes[process] if process in processes else ProcessChart(process)
+                curr_process.turn_on(timestamp)
+                for process_ in processes.values():
+                    if process_.get_name() != curr_process.get_name():
+                        process_.turn_off(timestamp)
+                processes[process] = curr_process
 
+            if event == "sched_switch:" and process == "<idle>":
+                next_task = data[-2].split(':')[0]
+                curr_process = processes[next_task] if next_task in processes else ProcessChart(next_task)
+                curr_process.turn_on(timestamp)
+                for process_ in processes.values():
+                    if process_.get_name() != curr_process.get_name():
+                        process_.turn_off(timestamp)
+                processes[next_task] = curr_process
+
+    return processes.values(), min_timestamp, max_timestamp
 
 def plot_set(process_charts, min_timestamp, max_timestamp):
-    fig, axis = plt.subplots(nrows=len(process_charts))
+    fig, axis = plt.subplots(nrows=len(process_charts), sharex='all')
     for idx, chart in enumerate(process_charts):
         axis[idx].plot(chart.get_axis_x(), chart.get_axis_y())
-        # axis[idx].
         axis[idx].set_title(chart.get_name())
-        print(chart.get_name(), '\n', chart.get_axis_x(), '\n', chart.get_axis_y())
-    plt.xlim([min_timestamp, max_timestamp])
-    plt.legend()
+        print(chart.get_name(), '\n', chart.get_axis_x(),
+              '\n', chart.get_axis_y())
+        axis[idx].set_xlim([min_timestamp, max_timestamp])
     plt.show()
 
 
 def main(argv):
-    data, min_timestamp, max_timestamp = conv_trace_result_from_file(argv[1])
-    process_chart = conv_trace_from_set_to_data_plot(data)
-    plot_set(process_chart, min_timestamp, max_timestamp)
+    processes, min_timestamp, max_timestamp = conv_trace_result_from_file(argv[1])
+    plot_set(processes, min_timestamp, max_timestamp)
 
 
 if __name__ == "__main__":
