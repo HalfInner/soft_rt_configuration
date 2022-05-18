fd70aaae027153d428633a3079c9e1ad4b60acfa
Half's Inner <kajbrz@hotmail.com>
2022-05-18 21:47:30 +0200

    work1

--- tasks/build.sh
+++ tasks/build.sh
@@ -7,7 +7,7 @@ OUTPUT_DIR=$1
 mkdir -p $OUTPUT_DIR
 
 printf "Building task_server... "
-g++ --std=c++2a $FLAGS -o $OUTPUT_DIR/task_server.a  $SCRIPT_PATH/task_print_server_high_prio.cc  -lrt &&\
+g++ --std=c++2a $FLAGS -o $OUTPUT_DIR/task_server.a  $SCRIPT_PATH/task_print_server_high_prio.cc  -lrt -pthread &&\
 printf "Done\n"
 
 printf "Building task_client_a... "
