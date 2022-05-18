d9a010d293f5ea04d419fa6e0d635c081374c126
Half's Inner <kajbrz@hotmail.com>
2022-05-18 22:21:09 +0200

    work1

---
+++ tasks/task_print_client_lower_prio.cc
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <array>
+#include <chrono>
+#include <cstddef>
+#include <cstdio>
+#include <iostream>
+#include <iterator>
+#include <numeric>
+#include <random>
+#include <thread>
+
+#include "naive_ipc.hh"
+#include "scoped_timer.hh"
+
+using namespace std::chrono_literals;
+int main(int argc, int argv[]) {
+  std::string client_name = "DEAFULT";
+  if (argc > 1) {
+    client_name = argv[1];
+  }
+  std::cout << "Starting client " << client_name << "...";
+  naive_ipc::MQ client{"/" + client_name};
+  client.initialize(naive_ipc::MQ::WorkPolicy::e_consumer);
+  std::cout << "OK\n";
+  while (true) {
+    {
+      auto t = HolidayBag::SportTimer("Client B", "us");
+      auto received_data_opt = client.receive();
+      if (received_data_opt) {
+        auto &received_data = received_data_opt.value();
+        std::sort(begin(received_data), end(received_data));
+        auto res = std::accumulate(
+            begin(received_data), end(received_data), 0,
+            [](auto sum, auto v) { return sum + static_cast<int>(v); });
+        (void)res;
+      }
+      t.stop();
+      if (received_data_opt) {
+        std::cout << t.getInterSummaryBag().unknit();
+      }
+    }
+    std::this_thread::sleep_for(100us);
+    // std::this_thread::yield();
+  }
+}
