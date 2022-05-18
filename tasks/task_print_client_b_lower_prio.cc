6efd8d231d0c35546d9336e121b4db59a5505be6
Half's Inner <kajbrz@hotmail.com>
2022-05-18 21:05:22 +0200

    work1

--- tasks/task_print_client_b_lower_prio.cc
+++ tasks/task_print_client_b_lower_prio.cc
@@ -24,7 +24,7 @@ int main() {
 
     std::sort(begin(received_data), end(received_data));
     auto res = std::accumulate(begin(received_data), end(received_data), 0,
-                               [](auto v) { return std::to_integer<int>(val); });
+                               [](auto v) { return std::to_integer<int>(v); });
     (void)res;
     // if (!received_data.empty()) {
     //   std::cout << "B:";
