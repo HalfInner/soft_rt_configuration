888a8cadf6bdc5078ddc7fbf10d07019752c64e2
Half's Inner <kajbrz@hotmail.com>
2022-05-19 01:38:49 +0200

    work1

--- tasks/task_print_client_lower_prio.cc
+++ tasks/task_print_client_lower_prio.cc
@@ -32,6 +32,6 @@ int main(int argc, char *argv[]) {
       (void)res;
     }
     // std::this_thread::yield();
-    std::this_thread::sleep_for(1ms);
+    std::this_thread::sleep_for(10ms);
   }
 }
