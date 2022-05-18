583816a34acf96f9b89c096951f4be3a4f0e23fe
Half's Inner <kajbrz@hotmail.com>
2022-05-19 00:00:55 +0200

    work1

--- tasks/task_print_server_high_prio.cc
+++ tasks/task_print_server_high_prio.cc
@@ -32,7 +32,7 @@ int main() {
   auto t = HolidayBag::SportTimer("Server", "us", finish_line);
   while (true) {
     {
-      constexpr size_t magic_number_to_get_1ms_load = 8000;
+      constexpr size_t magic_number_to_get_1ms_load = 4'096;
       std::shuffle(begin(arr), begin(arr) + magic_number_to_get_1ms_load, g);
       std::vector<std::byte> v;
       v.reserve(elements_to_send);
