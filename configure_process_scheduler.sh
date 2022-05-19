d52edda3e3819dac7ed609c6a7b986ee9f635033
Half's Inner <kajbrz@hotmail.com>
2022-05-19 02:08:02 +0200

    work1

--- configure_process_scheduler.sh
+++ configure_process_scheduler.sh
@@ -4,7 +4,7 @@
 
 function usage() {
     echo "Usage:"
-    echo "      $0 <test|configure>"
+    echo "      $0 <test|configure> <reverse>"
 }
 
 server_task_pid=$(ps aux | egrep -v "(bash|grep)" | grep  task_server | tr -s ' ' | cut -d ' ' -f 2)
@@ -12,6 +12,8 @@ client_a_task_pid=$(ps aux | egrep -v "(bash|grep)" | grep  task_client_A | tr -
 client_b_task_pid=$(ps aux | egrep -v "(bash|grep)" | grep  task_client_B | tr -s ' ' | cut -d ' ' -f 2)
 
 option=$1
+reverse_mode=$2
+
 if [[ "$option" == "test" ]]; then
     echo $(chrt -p $server_task_pid)
     echo $(chrt -p $client_a_task_pid)
@@ -20,9 +22,15 @@ elif [[ "$option" == "configure" ]]; then
     user_who=$(whoami)
     if [[ "$user_who" == "root" ]]; then 
         config='-f'
-        echo $(chrt $config -p 99 $server_task_pid)
-        echo $(chrt $config -p 98 $client_a_task_pid)
-        echo $(chrt $config -p 97 $client_b_task_pid)
+        if [[ "$reverse_mode" == "true" ]]; then
+            echo $(chrt $config -p 99 $server_task_pid)
+            echo $(chrt $config -p 98 $client_a_task_pid)
+            echo $(chrt $config -p 97 $client_b_task_pid)
+        else
+            echo $(chrt $config -p 99 $server_task_pid)
+            echo $(chrt $config -p 98 $client_a_task_pid)
+            echo $(chrt $config -p 97 $client_b_task_pid)
+        fi
     else
         echo "Script has to be executed by the 'root' but was by '$user_who'"
     fi 
