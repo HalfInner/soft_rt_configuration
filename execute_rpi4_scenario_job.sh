816be7fbaf93d94f37c2f2c1fd0969201f2e989a
Half's Inner <kajbrz@hotmail.com>
2022-05-19 23:53:33 +0200

    work1

---
+++ execute_rpi4_scenario_job.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/bash 
+# For more info: man trace-cmd
+
+user_who=$(whoami)
+if [[ "$user_who" != "root" ]]; then 
+    echo "Script has to be executed by the 'root' but was by '$user_who'"
+    exit 1
+fi
+
+tasks/clean.sh
+
+build_output_directory="output_rpi4"
+tasks/build.sh $build_output_directory
+
