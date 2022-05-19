f7532f7f28516a04f019e3f71f1f84aa5474be60
Half's Inner <kajbrz@hotmail.com>
2022-05-19 02:52:10 +0200

    work1

--- configure_process_scheduler.sh
+++ configure_process_scheduler.sh
@@ -39,7 +39,8 @@ elif [[ "$option" == "configure" ]]; then
         elif  [[ "$reverse_mode" == "deadline" ]]; then
             one_ms="1000000"
             hundred_us="10000"
-            config="-d -v  -T $hundred_us -D $one_ms -P $one_ms"
+            # config="-d -v  --sched-runtime $hundred_us --sched-deadline $one_ms --sched-period $one_ms"
+            config="-d -v $one_ms --sched-period $one_ms"
             printf "deadline\n"
             chrt $config -p 0 $server_task_pid
             chrt $config -p 0 $client_a_task_pid
