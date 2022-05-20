66c4ebac8e290dce17700deb62161c7e4d5842c9
Half's Inner <kajbrz@hotmail.com>
2022-05-20 18:50:51 +0200

    work1

--- execute_scheduler_tracer.sh
+++ execute_scheduler_tracer.sh
@@ -49,7 +49,11 @@ SIGINT=2
 kill -$SIGINT $tracer_pid
 if [[ "$include_stress" == "true" ]]; then
     printf "Stop stress... "
-    kill -$SIGINT $stress_pid
+    if [[ -z "$stress_pid" ]]; then 
+        ps aux | grep stress | tr -s ' ' | cut  -d ' ' -f 2 | xargs kill -$SIGINT
+    else
+        kill -$SIGINT $stress_pid
+    fi
     printf "Done\n"
 fi
 
